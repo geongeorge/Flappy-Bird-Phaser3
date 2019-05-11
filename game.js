eedecab65c76bfec0ca8c67afc315a2e69f057f7
Geon George <geongeorge98@gmail.com>
2019-05-11 20:36:28 +0530

    fixed pipe endless loop

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 550 },
             debug: false
         }
     },
@@ -22,25 +22,28 @@ function preload ()
 {
     this.load.image('sky', 'assets/sky.png');
     // this.load.image('star', 'assets/star.png');
-    this.load.image('pipe', 'assets/pipe2.png');
+    this.load.image('pipeb', 'assets/pipeb.png');
+    this.load.image('pipet', 'assets/pipet.png');
     this.load.spritesheet('birdy', 
         'assets/birdy.png',
         { frameWidth: 34, frameHeight: 24 }
     );
 }
 var platforms,spacebar,player;
-
+var gap = 150;
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
 //    this.add.image(400, 300, 'star');
     platforms = this.physics.add.staticGroup();
-
-    // platforms.setAll('body.immovable', true);
-    // platforms.setAll('body.velocity.x', 100);
-    platforms.create(game.canvas.width*1.5, getRandom(), 'pipe').setScale(1).refreshBody();
-    platforms.create(game.canvas.width*2, getRandom(), 'pipe').setScale(1).refreshBody();
+    // bottom placable at 260+gap to height
+    let pos = getRandom();
+    platforms.create(game.canvas.width*1.5, pos[0], 'pipeb').setScale(1).refreshBody();
+    platforms.create(game.canvas.width*1.5, pos[1], 'pipet').setScale(1).refreshBody();
+    pos = getRandom();
+    platforms.create(game.canvas.width*2, pos[0], 'pipeb').setScale(1).refreshBody();
+    platforms.create(game.canvas.width*2, pos[1], 'pipet').setScale(1).refreshBody();
 
     player = this.physics.add.sprite(100, 450, 'birdy');
 
@@ -68,13 +71,17 @@ function create ()
 }
 
 function getRandom() {
-    let min = Math.ceil(100);
-    let max = Math.floor(game.canvas.height-100);
+    let safePadding = 25;
+    let min = Math.ceil(safePadding+gap/2);
+    let max = Math.floor(game.canvas.height-safePadding-gap/2);
     let ran =  Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(ran)
-    return ran
+    let rantop = ran-((gap/2)+260);
+    let ranbot = ran+((gap/2)+260);
+    console.log(ranbot,rantop)
+    return [ranbot, rantop]
 }
 
+var countpipe = 0;
 function update ()
 {
     // if(spacebar.isDown) {
@@ -85,11 +92,21 @@ function update ()
     children.forEach((child) => {
         if (child instanceof Phaser.GameObjects.Sprite) {
             child.x += -3;
-
             if(child.x <= -50) {
-                console.log("Replacing child")
-                child.x = game.canvas.width+pipeWidth;
-                child.y = getRandom();
+                countpipe+=1;
+                console.log("Destroyed one "+countpipe)
+                child.destroy();
+
+                if(countpipe>=2) {
+                    pos = getRandom();
+                    console.log("created one")
+                    platforms.create(game.canvas.width+50, pos[0], 'pipeb').setScale(1).refreshBody();
+                    platforms.create(game.canvas.width+50, pos[1], 'pipet').setScale(1).refreshBody();
+                    countpipe=0;
+                }
+                
+                // child.x = game.canvas.width+pipeWidth;
+                // child.y = getRandom()[0];
             }
         }
     });
